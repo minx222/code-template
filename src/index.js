1fae225508972026b591904cd0bd2aaec579bdf2
Case <case@doocom.cn>
2024-08-05 13:46:18 +0800

    fix: 异常处理

--- src/index.js
+++ src/index.js
@@ -12,50 +12,59 @@ program
 .command("create")
 .description("基础模版")
 .action(async () => {
-	const appName = await input({
-		message: "请输入项目名称",
-		default: "app",
-	});
-	const template = await select({
-		message: "请选择初始化项目模板",
-		choices: reduceTemplate,
-	});
-
-	const config = templates[template];
-	config.appName = appName;
-	const isMonorepo = template === 'monorepo-base'
-
-	/**
-	 * @description 如果不是monorepo项目，就直接下载
-	 */
-	if(!isMonorepo) {
+	try {
+		const appName = await input({
+			message: "请输入项目名称",
+			default: "app",
+		});
+		const template = await select({
+			message: "请选择初始化项目模板",
+			choices: reduceTemplate,
+		});
+	
+		const config = templates[template];
+		config.appName = appName;
+		const isMonorepo = template === 'monorepo-base'
+	
+		/**
+		 * @description 如果不是monorepo项目，就直接下载
+		 */
+		if(!isMonorepo) {
+			run(config)
+			return;
+		}
+	
+		/**
+		 * @description 是否选择框架
+		 */
+		const isFramework = await confirm({
+			message: '是否选择一个代码框架',
+			default: true
+		})
+	
+		if(!isFramework) {
+			run(config)
+			return;
+		}
+	
+		const framework = await checkbox({
+			message: '请选择框架模版',
+			choices: reduceFramework
+		})
+	
+		config.children = framework.map(fra => {
+			return frameworks[fra]
+		})
+	
 		run(config)
-		return;
+	} catch (err) {
+		if(String(err).includes('ExitPromptError')) {
+			console.log(logSymbols.warning, chalk.yellow('用户取消操作'));
+		} else {
+			console.log(logSymbols.error, chalk.red(err));
+		}
 	}
 
-	/**
-	 * @description 是否选择框架
-	 */
-	const isFramework = await confirm({
-		message: '是否选择一个代码框架',
-		default: true
-	})
-
-	if(!isFramework) {
-		run(config)
-		return;
-	}
-
-	const framework = await checkbox({
-		message: '请选择框架模版',
-		choices: reduceFramework
-	})
-
-	config.children = framework.map(fra => {
-		return frameworks[fra]
-	})
-
-	run(config)
 });
 
 /**
